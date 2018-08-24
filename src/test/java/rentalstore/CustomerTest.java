ffe07d647aa3112a2db30af18280429e35eebcd6
XUHO3 <howells.xu@oocl.com>
2018-08-24 14:29:19 +0800

    update html_statement function test

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -22,13 +22,15 @@ public class CustomerTest {
     public void should_return_txt_statement_when_invoke_html_statement_function() {
 
         Customer customer=new Customer("howells");
+        Movie movie=new Movie("Roman Holiday",1);
+        Rental rental=new Rental(movie,3);
+        customer.addRental(rental);
 
         String result=  customer.htmlStatement();
 
         assertEquals("<H1>Rentals for <EM>howells</EM></H1><P>\n" +
                 "Roman Holiday: 3.0<BR>\n" +
-                "Titanic: 3.0<BR>\n" +
-                "<P>You owe<EM>6.0</EM><P>\n" +
-                "On this rental you earned <EM>2</EM> frequent renter points<P>",result);
+                "<P>You owe<EM>3.0</EM><P>\n" +
+                "On this rental you earned <EM>1</EM> frequent renter points<P>",result);
     }
 }
